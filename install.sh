956bec8b93692673ccbf27098c05bff26417f8d9
develz12 <julangtahtapratangga@gmail.com>
2025-04-17 14:42:02 +0000

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm install
+npm run dev
+composer install
+cp .env.example .env
+php artisan key:generate
+
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env &&
+sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
+
+php artisan migrate
+php artisan db:seed
+php artisan storage:link
+
