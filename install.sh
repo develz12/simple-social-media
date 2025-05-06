2de927f97f7b73bf7981544b35040274ffe8b799
develz12 <julangtahtapratangga@gmail.com>
2025-05-06 03:02:57 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
-set -e  # Stop script jika ada error
 
-# Install Node.js dependencies
 npm install
-
-# Build frontend (gunakan ini jika kamu memang perlu dev server)
-npm run dev &
-
-# Install PHP dependencies
+npm run dev
 composer install
-
-# Setup environment
-cp -n .env.example .env || true
+cp .env.example .env
 php artisan key:generate
 
-# Ubah konfigurasi databae
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/' .env
-sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/' .env
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env &&
+sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env &&
 
-# Migrasi dan seed database
-php artisan migrate --force
-php artisan db:seed --force
-
-# Buat symbolic link ke storage
+php artisan migrate
+php artisan db:seed
 php artisan storage:link
-
-# Jalankan server Laravel
-exec php artisan serve --host=0.0.0.0 --port=8000
-
